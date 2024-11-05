e2c135e6496ab1886d3fb6188240171954276523
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-05 10:49:41 -0300

    Add: File Input on New Dishes

--- src/database/knex/migrations/20241018005651_createDishes.js
+++ src/database/knex/migrations/20241018005651_createDishes.js
@@ -1,10 +1,11 @@
 exports.up = knex => knex.schema.createTable("dishes", table => {
     table.increments("id");
     table.integer("user_id").references("id").inTable("users");
-    table.text("name");
-    table.text("description");
-    table.decimal("price", 3, 2);
-    table.text("category");
+    table.text("name").notNullable();
+    table.text("description").notNullable();
+    table.decimal("price", 3, 2).notNullable();
+    table.text("category").notNullable();
+    table.text("image");
 
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+
 const DishesController = require("../controllers/DishesController");
+const DishesImageController = require("../controllers/DishesImageController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const dishesController = new DishesController();
+const dishesImageController = new DishesImageController();
 
 dishesRoutes.use(ensureAuthenticated);
 
@@ -12,5 +18,6 @@ dishesRoutes.get("/", dishesController.index);
 dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.patch("/image/:id", upload.single("image"), dishesImageController.update);
 
 module.exports = dishesRoutes;
